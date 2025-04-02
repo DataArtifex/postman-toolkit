ec725c0777b057aa3191f3b17a123a33e380aefe
Pascal Heus <pascal.heus@postman.com>
2025-04-02 17:19:03 -0600

    Adjusted tests

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+import pytest
+
+from dartfx.postmanapi import postman
+
+
+@pytest.fixture(scope="session", autouse=True)
+def test_dir(): 
+    return Path(__file__).parent
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    dotenv_path = Path(__file__).parent / ".env"  # Construct path from current test file dir
+    load_dotenv(dotenv_path=dotenv_path)
+
+@pytest.fixture(scope="session")
+def postman_api_key():
+    key = os.environ.get('DARTFX_POSTMAN_API_KEY')
+    if not key: # fallback to POSTMAN_API_KEY
+        key = os.environ.get('POSTMAN_API_KEY')
+    return key
+
+@pytest.fixture(scope="module")
+def postman_api(postman_api_key):
+    api = postman.PostmanApi(api_key=postman_api_key)
+    return api
+
+@pytest.fixture(scope="module")
+def postman_workspace(postman_api):
+    id = os.environ.get('DARTFX_POSTMAN_WS')
+    ws = postman.WorkspaceManager(postman_api, id)
+    return ws
+
--- tests/test_dataproductcollection.py
+++ tests/test_dataproductcollection.py
@@ -1,22 +1,10 @@
 from datetime import datetime
-from functools import cache
-import os
+from dartfx.postmanapi import postman
 
-from src.dartfx.postman import postman
-
-
-def get_api() -> postman.PostmanApi:
-    api = postman.PostmanApi(os.environ.get('POSTMAN_API_KEY'))
-    return api
-
-@cache
-def get_workspace(id="194dd33d-438d-47e1-ae69-e2ab5b414beb") -> postman.WorkspaceManager:
-    ws = postman.WorkspaceManager(get_api(), id)
-    return ws
-
-def test_create_dataproduct_101():
+def test_create_dataproduct_101(postman_api, postman_workspace):
     # unicode prefixes: ⛁ ⛃ 🔢
     # More at https://www.compart.com/en/unicode/category/So
-    collection_id = get_workspace().create_collection(f'🔢 Data Product 101 {datetime.now().isoformat()}', 'https://schema.getpostman.com/json/collection/v2.1.0/collection.json')
-    collection = postman.DataProductCollectionManager(get_api(), collection_id)
+    collection_id = postman_workspace.create_collection(f'🔢 Data Product 101 {datetime.now().isoformat()}', 'https://schema.getpostman.com/json/collection/v2.1.0/collection.json')
+    assert collection_id
+    collection = postman.DataProductCollectionManager(postman_api, collection_id)
     assert collection
